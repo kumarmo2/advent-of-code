43327fb7826bda59e3d07cd14f3cfe805f1fe1a4
kumarmo2 <kr.mohit6794@gmail.com>
2023-12-03 20:57:14 +0530

    day-3 part2 done.

--- 2023/day-3/src/main.rs
+++ 2023/day-3/src/main.rs
@@ -2,12 +2,157 @@
 
 fn main() {
     println!("Hello, world!");
-    println!("{}: ", part1(include_str!("../input-dev.txt")));
-    // let x = vec![5, 4, 32, 31, 1, 2, 3];
-    // println!("slice: {:?}", &x[1..1 + 2]);
+    println!("{}: ", part2(include_str!("../input-prod.txt")));
 }
 
-fn part1(input: &str) -> u32 {
+fn part2(input: &str) -> u64 {
+    let matrix = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    let mut aux: Vec<Vec<(u64, u64, u64)>> = Vec::new(); // the third item in the tuple signifies
+                                                         // the total number of adjacent numbers for the symbol
+    let rows = matrix.len();
+    let cols = matrix[0].len();
+
+    for i in 0..rows {
+        aux.push(vec![(0, 0, 0); cols]);
+    }
+
+    let mut i: usize = 0;
+    loop {
+        let mut j: usize = 0;
+        loop {
+            if j >= cols {
+                break;
+            }
+            let ch = matrix[i][j];
+            if !ch.is_digit(10) {
+                j += 1;
+                continue;
+            }
+            let (value, length) = get_int_val(&matrix[i][j..]);
+            let mut found_symbol = false;
+            if check_for_symbol_in_left(j, &matrix[i]) && matrix[i][j - 1] == '*' {
+                let mut tuple = aux[i][j - 1];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i][j - 1] = tuple;
+            }
+            if check_for_symbol_in_right(j, length, cols, &matrix[i])
+                && matrix[i][j + length] == '*'
+            {
+                let mut tuple = aux[i][j + length];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i][j + length] = tuple;
+            }
+
+            if check_for_symbol_in_top_left_diagonally(i, j, rows, cols, &matrix)
+                && matrix[i - 1][j - 1] == '*'
+            {
+                let mut tuple = aux[i - 1][j - 1];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i - 1][j - 1] = tuple;
+                // println!("...found_symbol on top  left");
+                // total = total + value;
+            }
+            if check_for_symbol_in_top_right_diagonally(i, j, length, rows, cols, &matrix)
+                && matrix[i - 1][j + length] == '*'
+            {
+                let mut tuple = aux[i - 1][j + length];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i - 1][j + length] = tuple;
+            }
+            if check_for_symbol_in_bottom_right_diagonally(i, j, length, rows, cols, &matrix)
+                && matrix[i + 1][j + length] == '*'
+            {
+                println!(">> found in diagonal");
+                let mut tuple = aux[i + 1][j + length];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i + 1][j + length] = tuple;
+            }
+            if check_for_symbol_in_bottom_left_diagonally(i, j, rows, cols, &matrix)
+                && matrix[i + 1][j - 1] == '*'
+            {
+                let mut tuple = aux[i + 1][j - 1];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i + 1][j - 1] = tuple;
+            }
+            let result = check_for_symbol_in_above_row_in_parallel(i, j, length, &matrix);
+            if result.0 && matrix[i - 1][result.1] == '*' {
+                let mut tuple = aux[i - 1][result.1];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i - 1][result.1] = tuple;
+            }
+            let result = check_for_symbol_in_below_row_in_parallel(i, j, rows, length, &matrix);
+            if result.0 && matrix[i + 1][result.1] == '*' {
+                let mut tuple = aux[i + 1][result.1];
+                tuple.2 = tuple.2 + 1;
+                if tuple.0 == 0 {
+                    tuple.0 = value
+                } else if tuple.1 == 0 {
+                    tuple.1 = value;
+                }
+                aux[i + 1][result.1] = tuple;
+            }
+            j = j + length;
+        }
+        i += 1;
+        if i >= rows {
+            break;
+        }
+    }
+
+    let total: u64 = aux
+        .into_iter()
+        .flatten()
+        .filter_map(|tuple| {
+            if tuple.2 != 2 {
+                return None;
+            }
+            Some(tuple.0 * tuple.1)
+        })
+        .sum();
+
+    // .collect::<Vec<(u64, u64, u64)>>();
+    total
+}
+
+fn part1(input: &str) -> u64 {
     let matrix = input
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
@@ -56,10 +201,10 @@ fn part1(input: &str) -> u32 {
             } else if check_for_symbol_in_bottom_left_diagonally(i, j, rows, cols, &matrix) {
                 println!("...found_symbol on bottom  left");
                 total = total + value;
-            } else if check_for_symbol_in_above_row_in_parallel(i, j, length, &matrix) {
+            } else if check_for_symbol_in_above_row_in_parallel(i, j, length, &matrix).0 {
                 println!("...found_symbol on above row in parallel");
                 total = total + value;
-            } else if check_for_symbol_in_below_row_in_parallel(i, j, rows, length, &matrix) {
+            } else if check_for_symbol_in_below_row_in_parallel(i, j, rows, length, &matrix).0 {
                 println!("...found_symbol on below row in parallel");
                 total = total + value;
             }
@@ -93,9 +238,9 @@ fn check_for_symbol_in_above_row_in_parallel(
     j: usize,
     length: usize,
     matrix: &Vec<Vec<char>>,
-) -> bool {
+) -> (bool, usize) {
     if i == 0 {
-        false
+        (false, 0)
     } else {
         check_for_symbol_in_row_in_parallel(i - 1, j, length, matrix)
     }
@@ -106,9 +251,9 @@ fn check_for_symbol_in_below_row_in_parallel(
     rows: usize,
     length: usize,
     matrix: &Vec<Vec<char>>,
-) -> bool {
+) -> (bool, usize) {
     if i == rows - 1 {
-        false
+        (false, 0)
     } else {
         check_for_symbol_in_row_in_parallel(i + 1, j, length, matrix)
     }
@@ -118,15 +263,15 @@ fn check_for_symbol_in_row_in_parallel(
     j: usize,
     length: usize,
     matrix: &Vec<Vec<char>>,
-) -> bool {
+) -> (bool, usize) {
     let mut k = j;
     while k < j + length {
         if is_symbol(matrix[row][k]) {
-            return true;
+            return (true, k);
         }
         k += 1;
     }
-    false
+    (false, 0)
 }
 
 fn check_for_symbol_in_top_left_diagonally(
@@ -203,7 +348,7 @@ fn is_symbol(ch: char) -> bool {
 }
 
 /// returns the numeric value and the length of the number.
-fn get_int_val(slice: &[char]) -> (u32, usize) {
+fn get_int_val(slice: &[char]) -> (u64, usize) {
     let length = slice.iter().take_while(|ch| ch.is_digit(10)).count();
     let mut multiplier = (10 as u32).pow(length as u32 - 1);
     let mut value: u32 = 0;
@@ -212,5 +357,5 @@ fn get_int_val(slice: &[char]) -> (u32, usize) {
         multiplier = multiplier / 10;
     }
     println!(">>> returning, value: {}, length: {}", value, length);
-    (value, length)
+    (value as u64, length)
 }
