330e43267897b28126813905e293ddf8775e59f0
kumarmo2 <kr.mohit6794@gmail.com>
2023-12-15 16:41:17 +0530

    day-12, part-1, clean code done.

--- 2023/d-12/src/main.rs
+++ 2023/d-12/src/main.rs
@@ -13,52 +13,45 @@ fn main() {
 }
 
 fn part1(input: &str) -> u32 {
-    let mut total = 0;
-    input.lines().for_each(|line| {
-        let mut splits = line.split(" ");
-        let fields = splits.next().unwrap();
-        let nums = splits
-            .next()
-            .unwrap()
-            .split(",")
-            .map(|num_str| num_str.parse::<u32>().unwrap())
-            .collect::<Vec<u32>>();
+    input
+        .lines()
+        .map(|line| {
+            let mut splits = line.split(" ");
+            let fields = splits.next().unwrap();
+            let nums = splits
+                .next()
+                .unwrap()
+                .split(",")
+                .map(|num_str| num_str.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
 
-        let arrangements = rec(fields, nums.as_slice(), &mut total);
-        // arrangements
-    });
-    total
+            let arrangements = rec(fields, nums.as_slice());
+            arrangements
+        })
+        .sum()
 }
 
-fn rec(fields: &str, nums: &[u32], total: &mut u32) {
+fn rec(fields: &str, nums: &[u32]) -> u32 {
     // println!("fields: {fields}, nums: {nums:?}");
     if nums.len() == 0 {
         if fields.contains("#") {
-            // println!("nums exhausted, but fields contains '#', returning 0");
-            // return 0;
-            return;
+            return 0;
         }
-        // println!("found a valid config, returning 1");
-        *total = *total + 1 as u32;
-        return;
+        return 1;
     }
 
     if fields.len() == 0 && nums.len() > 0 {
-        // return 0;
-        return;
+        return 0;
     }
 
     if fields.starts_with(".") {
-        // println!("found '.'. Going over it, fields: {fields}, nums: {nums:?}, total: {total}");
-        return rec(&fields[1..], nums, total);
+        return rec(&fields[1..], nums);
     }
 
     if fields.starts_with("#") {
         let mut count = 0;
         let mut local_fields = fields;
         // println!("found '#'. Iterating over it, fields: {fields}, nums: {nums:?}, total: {total}");
-
-        // while count < nums[0] {}
 
         while count < nums[0] {
             if local_fields.starts_with("#") || local_fields.starts_with("?") {
@@ -70,43 +63,27 @@ fn rec(fields: &str, nums: &[u32], total: &mut u32) {
         }
 
         if count < nums[0] {
-            // return 0;
             // println!(
             //     "count: {count} was less than nums[0]: {}...returning now",
             //     nums[0]
             // );
-            return;
+            return 0;
         }
 
         if local_fields.starts_with("#") {
             // count == nums[0] && local_fields starts with '#'
-            // return 0;
-            // println!("count was equal to nums[0], but next char was '#', ...returning now");
-            return;
+            return 0;
         }
         if local_fields.starts_with("?") {
-            return rec(&local_fields[1..], &nums[1..], total);
-            // if local_fields.len() == 1 {
-            //     // count == nums[0] && local_fields starts with '?'
-            //     // return 1;
-            //     println!("count was equal to nums[0], and next char was '?', ...returning now");
-            //     *total = *total + 1 as u32;
-            //     return;
-            // } else {
-            //     println!(
-            //         "count was equal to nums[0], and next char was '?' and len of local_fields > 1"
-            //     );
-            //     return rec(&local_fields[1..], &nums[1..], total);
-            // }
+            return rec(&local_fields[1..], &nums[1..]);
         }
-        return rec(local_fields, &nums[1..], total);
+        return rec(local_fields, &nums[1..]);
     }
 
     if fields.starts_with("?") {
-        // let mut count1 = 0;
         // println!("found '?', replacing with firstly with '.', fields: {fields}, nums: {nums:?}, total: {total}");
-        rec(&fields[1..], nums, total); // replaced '?' with  '.'
-                                        // println!("after replacing with '.', count: {count1}");
+        let count1 = rec(&fields[1..], nums); // replaced '?' with  '.'
+                                              // println!("after replacing with '.', count: {count1}");
 
         // println!("for previously found '?', replacing with secondly with '#', fields: {fields}, nums: {nums:?}, total: {total}");
         let mut count2 = 0;
@@ -122,30 +99,19 @@ fn rec(fields: &str, nums: &[u32], total: &mut u32) {
             }
         }
         if count2 < nums[0] {
-            // return count1;
             // println!("count2: {count2} was less than nums[0]: {}", nums[0]);
-            return;
+            return count1;
         }
         if local_fields.starts_with("#") {
             // count == nums[0] && local_fields starts with '#'
-            // return 0;
             // println!("count was equal to nums[0], but next char was '#', ...returning now");
-            return;
+            return count1;
         }
         if local_fields.starts_with("?") {
-            return rec(&local_fields[1..], &nums[1..], total);
-            // if local_fields.len() == 1 {
-            //     // count == nums[0] && local_fields starts with '?'
-            //     // return 1;
-            //     println!("count was equal to nums[0], and next char was '?', ...returning now");
-            //     *total = *total + 1 as u32;
-            //     return;
-            // } else {
-            //     return rec(&local_fields[1..], &nums[1..], total);
-            // }
+            return rec(&local_fields[1..], &nums[1..]) + count1;
         }
         // println!("count was equal to nums[0], and next char was not '?', local_fields: {local_fields:?}, ...returning now");
-        return rec(local_fields, &nums[1..], total);
+        return rec(local_fields, &nums[1..]) + count1;
     }
     unreachable!()
 }
