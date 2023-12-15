f28dcf4530dd1e8d39738c4ced4c2788d9b08ae1
kumarmo2 <kr.mohit6794@gmail.com>
2023-12-16 01:24:35 +0530

    added dotnet solution to day-12, part1.

---
+++ 2023/d-12/dotnet-solution/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace dotnet_solution;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var input = File.ReadAllText("../input-prod.txt");
+        Console.WriteLine(Part1(input));
+    }
+
+
+    private static uint Part1(string input)
+    {
+        var lines = input.Split("\n", StringSplitOptions.TrimEntries);
+        uint total = 0;
+        foreach (var line in lines)
+        {
+            if (line.Length == 0)
+            {
+                continue;
+            }
+            var index = line.IndexOf(' ');
+            ReadOnlySpan<char> lineSpan = line.Trim();
+            var fields = lineSpan.Slice(0, index);
+            var numsStrings = lineSpan.Slice(index + 1);
+            var nums = new List<uint>();
+
+            while (numsStrings.Length > 0)
+            {
+                int length = 0;
+                while (length < numsStrings.Length && numsStrings[length] != ',')
+                {
+                    length++;
+                }
+                var numString = numsStrings.Slice(0, length);
+                nums.Add(uint.Parse(numString));
+                if (length >= numsStrings.Length)
+                {
+                    break;
+                }
+                numsStrings = numsStrings.Slice(length + 1);
+            }
+            //
+            // Console.WriteLine($"fields: {fields}, length: {fields.Length}");
+            // Console.Write("nums: ");
+            // foreach (var num in nums)
+            // {
+            //     Console.Write(num + " ");
+            // }
+            // Console.WriteLine();
+            var x = CollectionsMarshal.AsSpan(nums).Slice(0);
+            total = total + Rec(fields, x);
+
+        }
+        return total;
+    }
+
+    static uint Rec(ReadOnlySpan<char> fields, ReadOnlySpan<uint> nums)
+    {
+        if (nums.Length == 0)
+        {
+            if (fields.Contains('#'))
+            {
+                return 0;
+            }
+            return 1;
+        }
+        if (fields.Length == 0 && nums.Length > 0)
+        {
+            return 0;
+        }
+
+        if (fields.StartsWith("."))
+        {
+            return Rec(fields.Slice(1), nums);
+        }
+
+        if (fields.StartsWith("#"))
+        {
+            var count = 0;
+            var localFields = fields;
+
+            while (count < nums[0])
+            {
+                if (localFields.StartsWith("#") || localFields.StartsWith("?"))
+                {
+                    count++;
+                    localFields = localFields.Slice(1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (count < nums[0])
+            {
+                return 0;
+            }
+
+            if (localFields.StartsWith("#"))
+            {
+                return 0;
+            }
+            if (localFields.StartsWith("?"))
+            {
+                return Rec(localFields.Slice(1), nums.Slice(1));
+            }
+            return Rec(localFields, nums.Slice(1));
+
+        }
+
+        if (fields.StartsWith("?"))
+        {
+
+            var count1 = Rec(fields.Slice(1), nums);
+
+            var count2 = 0;
+            var localFields = fields;
+            while (count2 < nums[0])
+            {
+                if (localFields.StartsWith("?") || localFields.StartsWith("#"))
+                {
+                    count2++;
+                    localFields = localFields.Slice(1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (count2 < nums[0])
+            {
+                return count1;
+            }
+            if (localFields.StartsWith("#"))
+            {
+                return count1;
+            }
+            if (localFields.StartsWith("?"))
+            {
+                return Rec(localFields.Slice(1), nums.Slice(1)) + count1;
+            }
+            return Rec(localFields, nums.Slice(1)) + count1;
+        }
+        // Console.WriteLine($"fields: {fields[0]}, length: {fields.Length}");
+        // Console.WriteLine($"nums: {nums}");
+        throw new NotImplementedException();
+    }
+}
--- 2023/d-12/src/main.rs
+++ 2023/d-12/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     println!("Hello, world!");
     let input = include_str!("../input-prod.txt");
     println!("part1: {:?}", part1(input))
+    // println!("part2: {:?}", part2(input))
 }
 
 fn part1(input: &str) -> u32 {
@@ -31,8 +32,38 @@ fn part1(input: &str) -> u32 {
         .sum()
 }
 
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut splits = line.split(" ");
+            let fields = splits.next().unwrap();
+            let nums = splits
+                .next()
+                .unwrap()
+                .split(",")
+                .map(|num_str| num_str.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
+
+            let mut agg_fields = String::new();
+            let mut agg_num: Vec<u32> = Vec::new();
+
+            for i in 0..5 {
+                agg_fields.push_str(fields);
+                if i != 4 {
+                    agg_fields.push('?');
+                }
+                for num in nums.iter() {
+                    agg_num.push(*num)
+                }
+            }
+            let arrangements = rec(&agg_fields, agg_num.as_slice());
+            arrangements
+        })
+        .sum()
+}
+
 fn rec(fields: &str, nums: &[u32]) -> u32 {
-    // println!("fields: {fields}, nums: {nums:?}");
     if nums.len() == 0 {
         if fields.contains("#") {
             return 0;
@@ -51,7 +82,6 @@ fn rec(fields: &str, nums: &[u32]) -> u32 {
     if fields.starts_with("#") {
         let mut count = 0;
         let mut local_fields = fields;
-        // println!("found '#'. Iterating over it, fields: {fields}, nums: {nums:?}, total: {total}");
 
         while count < nums[0] {
             if local_fields.starts_with("#") || local_fields.starts_with("?") {
@@ -63,15 +93,10 @@ fn rec(fields: &str, nums: &[u32]) -> u32 {
         }
 
         if count < nums[0] {
-            // println!(
-            //     "count: {count} was less than nums[0]: {}...returning now",
-            //     nums[0]
-            // );
             return 0;
         }
 
         if local_fields.starts_with("#") {
-            // count == nums[0] && local_fields starts with '#'
             return 0;
         }
         if local_fields.starts_with("?") {
@@ -81,17 +106,12 @@ fn rec(fields: &str, nums: &[u32]) -> u32 {
     }
 
     if fields.starts_with("?") {
-        // println!("found '?', replacing with firstly with '.', fields: {fields}, nums: {nums:?}, total: {total}");
         let count1 = rec(&fields[1..], nums); // replaced '?' with  '.'
-                                              // println!("after replacing with '.', count: {count1}");
 
-        // println!("for previously found '?', replacing with secondly with '#', fields: {fields}, nums: {nums:?}, total: {total}");
         let mut count2 = 0;
         let mut local_fields = fields;
         while count2 < nums[0] {
             if local_fields.starts_with("?") || local_fields.starts_with("#") {
-                // replacing '?'
-                // with '#' if encountered.
                 count2 += 1;
                 local_fields = &local_fields[1..];
             } else {
@@ -99,18 +119,14 @@ fn rec(fields: &str, nums: &[u32]) -> u32 {
             }
         }
         if count2 < nums[0] {
-            // println!("count2: {count2} was less than nums[0]: {}", nums[0]);
             return count1;
         }
         if local_fields.starts_with("#") {
-            // count == nums[0] && local_fields starts with '#'
-            // println!("count was equal to nums[0], but next char was '#', ...returning now");
             return count1;
         }
         if local_fields.starts_with("?") {
             return rec(&local_fields[1..], &nums[1..]) + count1;
         }
-        // println!("count was equal to nums[0], and next char was not '?', local_fields: {local_fields:?}, ...returning now");
         return rec(local_fields, &nums[1..]) + count1;
     }
     unreachable!()
